fdbce920823b1d0f86c1d350f77840233db2dbe1
billyyan <By869422>
2022-01-04 23:49:56 -0800

    Parse HTML Text - Jsoup

--- app/src/main/java/com/example/foody/models/Result.kt
+++ app/src/main/java/com/example/foody/models/Result.kt
@@ -1,9 +1,14 @@
 package com.example.foody.models
 
 
+import android.os.Parcelable
+import androidx.versionedparcelable.VersionedParcelize
 import com.example.foody.models.ExtendedIngredient
 import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
+import kotlinx.parcelize.RawValue
 
+@Parcelize
 data class Result(
     @SerializedName("aggregateLikes")
     val aggregateLikes: Int,
@@ -12,7 +17,7 @@ data class Result(
     @SerializedName("dairyFree")
     val dairyFree: Boolean,
     @SerializedName("extendedIngredients")
-    val extendedIngredients: List<ExtendedIngredient>,
+    val extendedIngredients: @RawValue List<ExtendedIngredient>,
     @SerializedName("glutenFree")
     val glutenFree: Boolean,
     @SerializedName("id")
@@ -35,4 +40,4 @@ data class Result(
     val vegetarian: Boolean,
     @SerializedName("veryHealthy")
     val veryHealthy: Boolean,
-)
+): Parcelable
---
+++ app/src/main/java/com/example/foody/ui/DetailsActivity.kt
@@ -0,0 +1,68 @@
+package com.example.foody.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.navigation.navArgs
+import com.example.foody.R
+import com.example.foody.adapter.PagerAdapter
+import com.example.foody.databinding.ActivityDetailBinding
+import com.example.foody.ui.fragment.ingredient.IngredientFragment
+import com.example.foody.ui.fragment.instructions.InstructionsFragment
+import com.example.foody.ui.fragment.overview.OverViewFragment
+import com.google.android.material.tabs.TabLayoutMediator
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class DetailsActivity : AppCompatActivity() {
+
+    private val args by navArgs<DetailsActivityArgs>()
+
+    private lateinit var binding: ActivityDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        binding.toolbar.setTitleTextColor(ContextCompat.getColor(this, R.color.white))
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val fragments = ArrayList<Fragment>()
+        fragments.add(OverViewFragment())
+        fragments.add(IngredientFragment())
+        fragments.add(InstructionsFragment())
+
+        val titles = ArrayList<String>()
+        titles.add("OverView")
+        titles.add("Ingredients")
+        titles.add("Instructions")
+
+        val resultBundle = Bundle()
+        resultBundle.putParcelable("recipeBundle", args.result)
+
+        val pagerAdapter = PagerAdapter(
+            resultBundle,
+            fragments,
+            this
+        )
+
+        binding.viewPager2.apply {
+            adapter = pagerAdapter
+        }
+
+        TabLayoutMediator(binding.tabLayout, binding.viewPager2) { tab, position ->
+            tab.text = titles[position]
+        }.attach()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == android.R.id.home) {
+            finish()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
